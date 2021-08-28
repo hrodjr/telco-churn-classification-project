a82177206df2222fd1430fa3919958b10b53b369
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2021-08-28 10:48:32 -0500

    updated project files

--- prepare.py
+++ prepare.py
@@ -9,19 +9,23 @@ def clean_data(df):
 #drop duplicates   
     df = df.drop_duplicates()
 #replaces empty values with 0
-    df = df.replace({'total_charges': ' '}, 0)
-#drop redundant columns
-    df = df.drop(['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], axis = 1)#,'senior_citizen','paperless_billing'
+    df = df.replace({'total_charges': ' '}, 0)#total_charges/tenure
+#drop redundant columns.
+#I dropped after reviewing the initial visualization with the understanding that I would be encoding "types" later in the pipeline.
+#Dropped paperless_billing because I felt it did not have an impact on churn.
+    df = df.drop(['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id', 'paperless_billing'], axis = 1)
 #change from object to float
     df['total_charges'] = df['total_charges'].astype(float)
-#Replaces 'no ... service' with 'No' for encoding.
+#Replaces 'no ... service' with 'No' for encoding. Customer can not have streaming or security services without internet. 
     df = df.replace(to_replace = 'No internet service', value = 'No')
     df = df.replace(to_replace = 'No phone service', value = 'No')
 
 #encode by creating a dummy df.
 #get_dummies creates a seperate df of booleans for the identified columns below. Cleaning for the decission tree.
-    dummy_df = pd.get_dummies(df[['churn',
+    dummy_df = pd.get_dummies(df[[
+    'churn',
     'gender',
+    'senior_citizen',
     'partner',
     'dependents',
     'phone_service',
@@ -31,35 +35,37 @@ def clean_data(df):
     'device_protection',
     'tech_support',
     'streaming_tv',
-    'streaming_movies',
-    'senior_citizen','paperless_billing']], dummy_na=False, drop_first=[True, True])
+    'streaming_movies']], dummy_na=False, drop_first=[True, True])
 
 #set 'drop_first' to 'False' to encode multiple types of the below listed columns.
     dummy_df_types = pd.get_dummies(df[['payment_type','internet_service_type','contract_type']], dummy_na=False, drop_first=False)
-
-#now drop the above two columns...
-    df = df.drop(columns=['dependents',
-    'phone_service',
-    'online_security',
-    'online_backup',
+    
+#drop encoded columns.   
+    df = df.drop(columns=[
+    'churn',
     'payment_type',
     'internet_service_type',
     'contract_type',
     'gender',
+    'senior_citizen',
     'partner',
+    'dependents',
+    'phone_service',
     'multiple_lines',
+    'online_security',
+    'online_backup',
     'device_protection',
     'tech_support',
     'streaming_tv',
-    'streaming_movies',
-    'churn',
-    'senior_citizen','paperless_billing'])
+    'streaming_movies'])
 
 #...and concatanate the dummies df with the prep's df.
     df = pd.concat([df, dummy_df_types, dummy_df], axis=1)
 
-#rename columns
-    df = df.rename(columns={'payment_type_Bank transfer (automatic)':'bank_transfer',
+#rename columns newly encoded columns
+    df = df.rename(columns={
+    'churn_Yes':'churned',
+    'payment_type_Bank transfer (automatic)':'bank_transfer',
     'payment_type_Credit card (automatic)':'credit_card',
     'payment_type_Electronic check':'e_check',
     'payment_type_Mailed check':'check',
@@ -69,37 +75,35 @@ def clean_data(df):
     'contract_type_Month-to-month':'m2m',
     'contract_type_One year':'one_year_contract',
     'contract_type_Two year':'two_year_contract',
-    'dependents_Yes':'dependents',
-    'phone_service_Yes':'phone_service',
-    'online_security_Yes':'online_security',
-    'online_backup_Yes':'online_backup',
+    'senior_citizen':'is_senior',
     'gender_Male':'is_male',
     'partner_Yes':'partner',
+    'dependents_Yes':'dependents',
+    'phone_service_Yes':'phone_service',
     'multiple_lines_Yes':'multiple_lines',
+    'online_security_Yes':'online_security',
+    'online_backup_Yes':'online_backup',
     'device_protection_Yes':'device_protection',
     'tech_support_Yes':'tech_support',
     'streaming_tv_Yes':'streaming_tv',
-    'streaming_movies_Yes':'streaming_movies',
-    'paperless_billing_Yes':'paperless',
-    'churn_Yes':'churned',
-    'senior_citizen_yes':'senior_citizen'})
+    'streaming_movies_Yes':'streaming_movies'})
 
-#after encoding the decision to concatanate bank_transfer and credit_card into auto_payment was made to whether or not autopayments had a play on churn.    
-#combine 'bank_transfer' and 'credit_card' with new 'auto_payment' column
-   # df['autopayment'] = df['bank_transfer'] + df['credit_card']
-   # df['not_autopayment'] = df['e_check'] + df['check']
+#I decided change the following variables into single features.
+    df['autopayment'] = df['bank_transfer'] + df['credit_card']
 
-#after reviewing the autopayment and nonautopayment I want to combine the two to be autopayment (yes or no). 
-    #df['auto_payment'] = df['autopayment'] + df['not_autopayment']
-
-#drop 'bank_transfer' and 'credit_card' columns
-    #df = df.drop(columns=['bank_transfer', 'credit_card', 'e_check', 'check'])
+#drop original of new features.
+#removed total charges. The focus here is on how monthly charges may or may no affect churn rates.
+    df = df.drop(columns=['bank_transfer', 'credit_card', 'e_check', 'check', 'one_year_contract', 'two_year_contract', 'no_internet', 'dsl', 'total_charges'])
 
 #dropped the following columns after running univariate and bivariate statistics
     #df = df.drop(columns=['is_male', 'online_security', 'online_backup', 'device_protection', 'one_year_contract', 'two_year_contract', 'multiple_lines', 'total_charges', 'autopayment', 'not_autopayment', 'streaming_tv', 'streaming_movies'])
 
 #convert uint8 to int64 for astethics.
-    #df = df.astype({'dsl':'int64',
+    #df = df.astype({
+    #'bank_transfer',
+    #
+    # 
+    #'dsl':'int64',
  #   'fiber':'int64',
  #   'no_internet':'int64',
   #  'm2m':'int64',
