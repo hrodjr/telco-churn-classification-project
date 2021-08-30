2344e45d7d249eb3a21e2ed584750e7b54ec97d1
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2021-08-29 19:53:30 -0500

    updated project file

--- explore.py
+++ explore.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 from scipy import stats
 
 def train_validate_test_split(df, target, seed=123):
@@ -217,3 +219,26 @@ def plot_swarm_grid_with_color(train, target, cat_vars, quant_vars):
             ax[i].set_ylabel(quant)
             ax[i].set_title(cat)
         plt.show()
+
+#prediction, accuracy and class report evaluation function used for the above functions
+def get_metrics_bin(clf, X, y):
+    '''
+    get_metrics_bin will take in a sklearn classifier model, an X and a y variable and utilize
+    the model to make a prediction and then gather accuracy, class report evaluations
+    Credit to @madeleine-capper
+    return:  a classification report as a pandas DataFrame
+    '''
+    y_pred = clf.predict(X)
+    accuracy = clf.score(X, y)
+    conf = confusion_matrix(y, y_pred)
+    class_report = pd.DataFrame(classification_report(y, y_pred, output_dict=True)).T
+    tpr = conf[1][1] / conf[1].sum()
+    fpr = conf[0][1] / conf[0].sum()
+    tnr = conf[0][0] / conf[0].sum()
+    fnr = conf[1][0] / conf[1].sum()
+    print(f'''
+    The accuracy for our model is {accuracy:.4}
+    The True Positive Rate is {tpr:.3}, The False Positive Rate is {fpr:.3},
+    The True Negative Rate is {tnr:.3}, and the False Negative Rate is {fnr:.3}
+    ''')
+    return class_report     
